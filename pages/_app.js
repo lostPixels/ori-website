5d8271071b818f1b19972b0dd85f57451b0de40a
James Merrill <james.merrills@onlogic.com>
2022-12-18 09:31:36 -0500

    Wired up backend data.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <h1>LOL</h1>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
