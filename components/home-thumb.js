e2f8d371a17b65b59730cbf69cc53dd6e9b32599
James Merrill <james.merrills@onlogic.com>
2022-12-20 11:10:10 -0500

    homepage animation

--- components/home-thumb.js
+++ components/home-thumb.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const HomeThumb = props => {
 
+    const [cn, setCN] = useState('opacity-0');
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        let int = requestAnimationFrame(() => setCN(''));
+        return () => cancelAnimationFrame(int)
+    });
+
     return (
-        <div className={`${props.className} border border-slate-100 m-4`}>
-            <img src={props.attributes.formats.medium.url} width={392} height={522} />
+        <div className={`${props.className} border border-slate-100 m-4 transition ease-in-out delay-${props.index * 100} duration-1000 ${cn}`}>
+            <img src={props.attributes.formats.medium.url} width={392} height={522} onLoad={() => setLoaded(true)} />
         </div>
     )
 }
